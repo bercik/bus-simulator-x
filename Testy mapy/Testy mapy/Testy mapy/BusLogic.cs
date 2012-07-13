b53b122260c388f6360c03858447daf1eadd8b9e
filip.borkiewicz <boreq@sourcedrops.com>
2012-07-13 12:37:21 +0000

    BusLogic dodane, praktycznie nic tam nie ma

---
+++ Testy mapy/Testy mapy/Testy mapy/BusLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Testy_mapy
+{
+    class BusLogic
+    {
+        private Vector2 position;
+        private float direction;
+        private float speed;
+        private float sideAcc;
+        private float width;
+        private float height;
+
+        public BusLogic(float x, float y)
+        {
+            this.position.X = x;
+            this.position.Y = y;
+        }
+
+        public Vector2 GetBusPosition()
+        {
+            Vector2 busPosition;
+
+            busPosition.X = position.X + (height * float(Math.Sin(direction)) / 2);
+            busPosition.Y = ;
+
+            return busPosition;
+        }
+    }
+}
