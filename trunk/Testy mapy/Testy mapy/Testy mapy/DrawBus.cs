65c77d9b47bd63bf98a724215b3ad9289f92519f
robert.cebula1 <robert.cebula1@958e2df3-88bc-7935-9341-dcf9a950dca5>
2012-07-14 13:41:40 +0000

    Dodana metoda rysująca beczke na podanej pozycji względem lewego górnego rogu ekranu (plik DrawBus.cs klasa DrawBus metoda Draw())

--- trunk/Testy mapy/Testy mapy/Testy mapy/DrawBus.cs
+++ trunk/Testy mapy/Testy mapy/Testy mapy/DrawBus.cs
@@ -15,34 +15,21 @@ namespace Testy_mapy
 {
     class DrawBus
     {
-        SpriteBatch spriteBatch;
+        Texture2D busTexture;
 
         public DrawBus()
         {
 
         }
 
-        public void LoadContent(GraphicsDevice graphicsDevice)
+        public void LoadContent(ContentManager content)
         {
-            spriteBatch = new SpriteBatch(graphicsDevice);
+            busTexture = content.Load<Texture2D>("beczka");
         }
 
-        public void DrawPoints(List<Vector2> points, Vector2 screenPos, Vector2 screenSize)
+        public void Draw(SpriteBatch spriteBatch, Vector2 pos)
         {
-            spriteBatch.Begin();
-
-            foreach (Vector2 point in points)
-            {
-                Point p_point = new Point();
-                p_point.X = (int)(point.X - (screenPos.X - screenSize.X / 2));
-                p_point.Y = (int)(point.Y - (screenPos.Y - screenSize.Y / 2));
-
-                Rectangle rect = new Rectangle(p_point.X, p_point.Y, 4, 4);
-                Color color = Color.White;
-                Primitives2D.FillRectangle(spriteBatch, rect, color);
-            }
-
-            spriteBatch.End();
+            spriteBatch.Draw(busTexture, pos, Color.White);
         }
     }
 }
