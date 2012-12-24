ca6fb815b919bc66c167c31fbab5ad157b95d7a5
robert.cebula1 <robert.cebula1@958e2df3-88bc-7935-9341-dcf9a950dca5>
2012-12-24 18:39:48 +0000

    $ Poprawki związane z publikowaniem gry

--- Testy mapy/Testy mapy/Testy mapy/GameplayLogic.cs
+++ Testy mapy/Testy mapy/Testy mapy/GameplayLogic.cs
@@ -828,52 +828,47 @@ namespace Testy_mapy
             /// </summary>
             public List<BusStop> LoadMap(string filePath)
             {
-                filePath = "maps/" + filePath;
                 List<BusStop> tempBusStops = new List<BusStop>();
+                // Przygotuj StreamReader.
+                Stream s = TitleContainer.OpenStream("Content/maps/" + filePath);
+                Encoding encoding = Encoding.GetEncoding("Windows-1250");
+                StreamReader streamReader = new StreamReader(s, encoding);
 
-                if (File.Exists(filePath))
+                // Zmienna pozwalająca na ustalenie czy czas rozpocząć odczytywanie.
+                bool beginningEncountered = false;
+
+                while (true)
                 {
-                    // Przygotuj StreamReader.
-                    FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-                    Encoding encoding = Encoding.GetEncoding("Windows-1250");
-                    StreamReader streamReader = new StreamReader(fileStream, encoding);
+                    // Odczytaj nową linię.
+                    string line = streamReader.ReadLine();
 
-                    // Zmienna pozwalająca na ustalenie czy czas rozpocząć odczytywanie.
-                    bool beginningEncountered = false;
-
-                    while (true)
+                    if (line == null)
                     {
-                        // Odczytaj nową linię.
-                        string line = streamReader.ReadLine();
+                        break;
+                    }
 
-                        if (line == null)
+                    // Jeśli rozpoczęto odczyt.
+                    if (beginningEncountered)
+                    {
+                        // Jeśli należy zakończyć odczyt, wyjdź z pętli (koniec tekstu v koniec sekcji bus_stops).
+                        if (line[0] == '*')
                         {
                             break;
                         }
 
-                        // Jeśli rozpoczęto odczyt.
-                        if (beginningEncountered)
+                        // Jeśli odczytano tekst niebędący komentarzem.
+                        if (!TextIsComment(line))
                         {
-                            // Jeśli należy zakończyć odczyt, wyjdź z pętli (koniec tekstu v koniec sekcji bus_stops).
-                            if (line[0] == '*')
-                            {
-                                break;
-                            }
+                            string[] split = line.Split(new char[] { ';' });
+                            BusStop busStop = new BusStop(int.Parse(split[0]), new Vector2(float.Parse(split[1]), float.Parse(split[2])), int.Parse(split[3]), new Vector2(float.Parse(split[4]), float.Parse(split[5])), int.Parse(split[6]), new Vector2(float.Parse(split[7]), float.Parse(split[8])), int.Parse(split[9]));
 
-                            // Jeśli odczytano tekst niebędący komentarzem.
-                            if (!TextIsComment(line))
-                            {
-                                string[] split = line.Split(new char[] { ';' });
-                                BusStop busStop = new BusStop(int.Parse(split[0]), new Vector2(float.Parse(split[1]), float.Parse(split[2])), int.Parse(split[3]), new Vector2(float.Parse(split[4]), float.Parse(split[5])), int.Parse(split[6]), new Vector2(float.Parse(split[7]), float.Parse(split[8])), int.Parse(split[9]));
-
-                                tempBusStops.Add(busStop);
-                            }
+                            tempBusStops.Add(busStop);
                         }
+                    }
 
-                        if (line == "*busstops*")
-                        {
-                            beginningEncountered = true;
-                        }
+                    if (line == "*busstops*")
+                    {
+                        beginningEncountered = true;
                     }
                 }
 
@@ -885,26 +880,22 @@ namespace Testy_mapy
             /// </summary>
             public List<Int32> LoadOrder(string filePath)
             {
-                filePath = "maps/" + filePath;
                 List<Int32> tempOrder = new List<Int32>();
 
-                if (File.Exists(filePath))
+                // Przygotuj StreamReader.
+                Stream s = TitleContainer.OpenStream("Content/maps/" + filePath);
+                Encoding encoding = Encoding.GetEncoding("Windows-1250");
+                StreamReader streamReader = new StreamReader(s, encoding);
+
+                string line;
+
+                // Przeliteruj przez wszystkie linie przypisując je do line.
+                while ((line = streamReader.ReadLine()) != null)
                 {
-                    // Przygotuj StreamReader.
-                    FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-                    Encoding encoding = Encoding.GetEncoding("Windows-1250");
-                    StreamReader streamReader = new StreamReader(fileStream, encoding);
-
-                    string line;
-
-                    // Przeliteruj przez wszystkie linie przypisując je do line.
-                    while ((line = streamReader.ReadLine()) != null)
+                    // Jeśli odczytano tekst niebędący komentarzem.
+                    if (!TextIsComment(line))
                     {
-                        // Jeśli odczytano tekst niebędący komentarzem.
-                        if (!TextIsComment(line))
-                        {
-                            tempOrder.Add(Int32.Parse(line));
-                        }
+                        tempOrder.Add(Int32.Parse(line));
                     }
                 }
 
