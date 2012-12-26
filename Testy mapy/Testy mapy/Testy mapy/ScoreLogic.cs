d27db18791b3fdffb31f0cba057f88d310d5e4a2
robert.cebula1 <robert.cebula1@958e2df3-88bc-7935-9341-dcf9a950dca5>
2012-12-26 12:06:45 +0000

    $ Poprawione znikanie strzałki na predkosciomierzu przy zmianie rozmiaru HUD'u + Dodany system punktów (Akcje dodajemy w konstruktorze klasy Score, a potem wywolujemy poprzez Score.AddAction) + Wyświetlanie aktualnej akcjii + Wyświetlanie wyniku

---
+++ Testy mapy/Testy mapy/Testy mapy/ScoreLogic.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Testy_mapy
+{
+    public struct Action
+    {
+        public string message { get; private set; } // wiadomosc
+        public int points { get; private set; } // ilosc punktow
+
+        public Action(int points, string message) : this()
+        {
+            this.points = points;
+            this.message = message;
+        }
+
+        public Action(Action action, float multiplier) : this()
+        {
+            this.points = (int)(action.points * multiplier);
+            this.message = action.message + ": " + points.ToString() + " pkt.";
+        }
+    }
+
+    public static class Score
+    {
+        private static float totalScore = 0.0f; // całkowity wynik
+        private static float addedScore = 0.0f; // liczba dodanych punktów
+        private static float removedScore = 0.0f; // liczba odjętych punktów
+
+        private static Dictionary<string, Action> actions; // slownik mozliwych akcjii
+        private static List<Action> currentActions; // lista obecnych akcjii
+
+        static Score()
+        {
+            actions = new Dictionary<string, Action>();
+            currentActions = new List<Action>();
+
+            // DODAJ AKCJE TUTAJ:
+            actions.Add("killed pedestrian", new Action(-100, "Zabicie pieszego"));
+            actions.Add("click pause", new Action(50, "Klikniecie pauzy"));
+        }
+
+        public static int GetAddedScore()
+        {
+            return (int)addedScore;
+        }
+
+        public static int GetRemovedScore()
+        {
+            return (int)removedScore;
+        }
+
+        public static int GetTotalScore()
+        {
+            return (int)totalScore;
+        }
+
+        public static void AddAction(string actionName, float multiplier)
+        {
+            Action action = new Action(actions[actionName], multiplier);
+
+            if (action.points < 0)
+                removedScore -= action.points;
+            else
+                addedScore += action.points;
+
+            totalScore += action.points;
+
+            currentActions.Add(action);
+        }
+
+        public static bool GetNextAction(out string currentMessage, out Color currentColor)
+        {
+            if (currentActions.Count > 0)
+            {
+                currentMessage = currentActions[0].message;
+                currentColor = (currentActions[0].points < 0) ? Color.Red : Color.Green;
+
+                currentActions.RemoveAt(0);
+
+                return true;
+            }
+            else
+            {
+                currentMessage = "";
+                currentColor = Color.White;
+
+                return false;
+            }
+        }
+    }
+}
