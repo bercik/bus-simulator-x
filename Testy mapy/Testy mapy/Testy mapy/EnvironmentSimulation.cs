df80cda90f644544f1e411c355042792c5fde97a
robert.cebula1 <robert.cebula1@958e2df3-88bc-7935-9341-dcf9a950dca5>
2013-01-21 17:38:18 +0000

    + Dodany zachód słońca + Dodane losowe występowanie deszczu $ Poprawiony cykl dnia i nocy $ Poprawione prostokąty czerwonych świateł

--- Testy mapy/Testy mapy/Testy mapy/EnvironmentSimulation.cs
+++ Testy mapy/Testy mapy/Testy mapy/EnvironmentSimulation.cs
@@ -19,18 +19,33 @@ namespace Testy_mapy
 
         private double time = 0.0f; // czas który upłynął od ostatnio dodanej minuty
 
+        // zachod slonca:
+        private bool sunset; // czy wystepuje efekt zachodu slonca
+        private bool setSunset; // czy wylosowano juz czy ma nastapic efekt zachodu slonca
+
+        // deszcz:
+        private bool rain; // czy pada deszcz
+        private int weatherChangeTimer; // ile czasu uplynelo od zmiany pogody (w minutach)
+        private int weatherChangeTime; // ile czasu ma trwac dana pogoda (w minutach)
+
         public EnvironmentSimulation()
         {
             hour = 0;
             minute = 0;
+
+            setSunset = false;
+            sunset = false;
+            rain = false;
+            weatherChangeTime = 0;
+            weatherChangeTimer = 0;
         }
 
-        public EnvironmentSimulation(int minute, int hour)
+        public EnvironmentSimulation(int minute, int hour) : this()
         {
             SetTime(minute, hour);
         }
 
-        public void Update(TimeSpan frameInterval)
+        public void Update(TimeSpan frameInterval, ref ParticlesLogic particlesLogic)
         {
             time += frameInterval.TotalSeconds;
 
@@ -38,6 +53,9 @@ namespace Testy_mapy
             {
                 AddMinute();
                 time -= GameParams.timeBeetwenOneMinute;
+
+                SetSunset();
+                SetRain(ref particlesLogic);
             }
         }
 
@@ -54,18 +72,92 @@ namespace Testy_mapy
 
         public Vector4 GetGlobalLightColor()
         {
-            float radians = MathHelper.ToRadians((hour + minute * 0.01666f) * 15);
-            float value = (float)Math.Abs(Math.Sin(radians / 2));
+            float red = 1.0f, greenBlue = 1.0f;
 
-            if (value < 0.1f)
-                value = 0.1f;
+            float fHour = hour + (minute * 0.0166f);
 
-            return new Vector4(value, value, value, 1);
+            if (sunset)
+            {
+                if (fHour >= 18 && fHour <= 22.5f)
+                    red = -0.177f * fHour + 4.2f;
+                else if (fHour > 22.5f || fHour < 3)
+                    red = 0.2f;
+
+                if (fHour >= 18 && fHour <= 21)
+                    greenBlue = -0.266f * fHour + 5.8f;
+                else if (fHour > 21 || fHour < 3)
+                    greenBlue = 0.2f;
+            }
+            else if (!sunset)
+            {
+                if (fHour >= 18 && fHour <= 21)
+                    red = greenBlue = -0.266f * fHour + 5.8f;
+                else if (fHour > 21 || fHour < 3)
+                    red = greenBlue = 0.2f;
+            }
+
+            if (fHour >= 3 && fHour <= 6)
+            {
+                red = greenBlue = 0.266f * fHour - 0.6f;
+            }
+            else if (fHour > 6 && fHour < 18)
+            {
+                red = greenBlue = 1.0f;
+            }
+
+            return new Vector4(red, greenBlue, greenBlue, 1);
+        }
+
+        private void SetSunset()
+        {
+            if (hour == 17 && !setSunset)
+            {
+                if (rain)
+                {
+                    sunset = false;
+                }
+                else
+                {
+                    if (Helper.random.NextDouble() < GameParams.sunsetProbability)
+                        sunset = true;
+                    else
+                        sunset = false;
+                }
+
+                setSunset = true;
+            }
+            if (hour == 18)
+            {
+                setSunset = false;
+            }
+        }
+
+        private void SetRain(ref ParticlesLogic particlesLogic)
+        {
+            if (!(sunset && hour >= 18 && hour <= 22) && weatherChangeTimer > weatherChangeTime)
+            {
+                // ustawiamy liczniki:
+                weatherChangeTimer = 0;
+                weatherChangeTime = Helper.random.Next(GameParams.minWeatherChangeTime, GameParams.maxWeatherChangeTime);
+
+                // losujemy pogode:
+                if (Helper.random.NextDouble() < GameParams.rainProbability)
+                {
+                    rain = true;
+                    particlesLogic.EnableRain();
+                }
+                else
+                {
+                    rain = false;
+                    particlesLogic.DisableRain();
+                }
+            }
         }
 
         private void AddMinute()
         {
             ++minute;
+            ++weatherChangeTimer;
 
             if (minute > 59)
             {
